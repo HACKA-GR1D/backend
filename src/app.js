8e3e9ae709f6a47ad6ea95e64fc49e9b872f5a8f
ETomacheski <tomacheski2001@gmail.com>
2020-08-22 23:20:12 -0300

    configurando swagger

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const swaggerUi = require('swagger-ui-express')
 
 const app = express()
 const cors = require('cors')
+const openApiDocumentation = require('../swagger.json')
 
 app.use(express.json())
 
+app.use('/', swaggerUi.serve, swaggerUi.setup(openApiDocumentation))
 const routes = require('./routes')
 app.use(cors({
   credentials: true,
